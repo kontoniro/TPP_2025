68b8b79795433401a92ca70b35ab22ab4131a30a
reikary <sabberg555@gmail.com>
2025-11-07 11:02:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -28,45 +28,7 @@ class Polynomial
     }
 
     public override string ToString()
-    {
-        for (int i = 0; i < coeffs.Length; i++)
-        {
-            double currentCoeff = coeffs[i];
-        if (Math.Abs(currentCoeff) < double.Epsilon)
-            {
-                continue;
-            }
-        if (currentCoeff > 0)
-         if (!isFirstTerm)
-                {
-                    sb.Append(" + ");
-                }
-            }
-            else
-            {
-                sb.Append(" - ")
-            }
-            string coeffString = currentCoeff.ToString();
-            if (i == 0)
-            {
-                sb.Append(coeffString);
-            }
-            else if (i == 1)
-            {
-                if (currentCoeff != 1.0)
-                {
-                    sb.Append(coeffString);
-                }
-                sb.Append("x");
-            }
-            else
-            {
-                if (currentCoeff != 1.0)
-                {
-                    sb.Append(coeffString);
-                }
-                sb.Append("x^").Append(i);
-            }
+???????????????????????????????????????????????????????????????????????????????????????
         return "";
     }
 }
@@ -80,4 +42,5 @@ class Programm
 
         Console.WriteLine(p);
     }
+
 }
